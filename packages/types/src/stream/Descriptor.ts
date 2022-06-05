147e6a25cf83e61ca43b69d673a905fb3c54e065
Adrien Belminksy <belminksy@gmail.com>
2022-06-06 00:56:53 +0200

    feat(types): add stream types

---
+++ packages/types/src/stream/Descriptor.ts
@@ -0,0 +1,16 @@
+/**
+ * Copyright (c) Minimouli
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+enum Descriptor {
+    STDIN = 0,
+    STDOUT = 1,
+    STDERR = 2
+}
+
+export {
+    Descriptor
+}
---
+++ packages/types/src/stream/index.ts
@@ -0,0 +1,14 @@
+/**
+ * Copyright (c) Minimouli
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+export * from './Descriptor.js'
+export * from './Duplex.js'
+export * from './NativeReadable.js'
+export * from './NativeWritable.js'
+export * from './Readable.js'
+export * from './Stream.js'
+export * from './Writable.js'
