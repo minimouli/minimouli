27707e02b8acb36ae21d31009fd2c12147e463a7
Adrien Belminksy <belminksy@gmail.com>
2022-06-25 19:10:56 +0200

    chore: use babel instead of ts-jest

---
+++ babel.config.js
@@ -0,0 +1,20 @@
+/**
+ * Copyright (c) Minimouli
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+module.exports = {
+    presets: [
+        [
+            '@babel/preset-env',
+            {
+                targets: {
+                    node: 'current'
+                }
+            }
+        ],
+        '@babel/preset-typescript'
+    ],
+}
