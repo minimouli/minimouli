9de877e4e9d3db4b9b2b6ec6736c56a03f449d0e
Adrien Belminksy <belminksy@gmail.com>
2022-07-04 17:10:39 +0200

    feat(types): add block types

---
+++ packages/types/src/blocks/index.ts
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) Minimouli
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import type { MatcherShape } from '../matchers'
+import type { ms } from '../Unit.js'
+
+type BlockFn = () => void
+type ConcurrentBlockFn = () => Promise<void>
+
+type SuiteFn = BlockFn
+type TestFn = ConcurrentBlockFn | BlockFn
+type HookFn = BlockFn
+
+type Suite = (name: string, fn: SuiteFn) => void
+type Test = (name: string, fn: TestFn) => void
+type Hook = (fn: HookFn) => void
+type Sleep = (duration: ms) => Promise<void>
+type Expect = <R = unknown>(received: R) => MatcherShape<R>
+
+export type {
+    BlockFn,
+    ConcurrentBlockFn,
+    SuiteFn,
+    TestFn,
+    HookFn,
+    Suite,
+    Test,
+    Hook,
+    Sleep,
+    Expect
+}
