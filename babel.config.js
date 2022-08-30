8bc9f11cd144a31139c2160f03d4a33251ae38ee
Adrien Belminksy <belminksy@gmail.com>
2022-08-31 01:03:20 +0200

    build: add production build with babel

--- babel.config.js
+++ babel.config.js
@@ -5,7 +5,26 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-module.exports = {
+const buildConfig = {
+    presets: [
+        [
+            '@babel/preset-env',
+            {
+                targets: 'node 12.20.0',
+                useBuiltIns: 'usage',
+                corejs: '3.25',
+                modules: false
+            }
+        ],
+        '@babel/preset-react',
+        '@babel/preset-typescript'
+    ],
+    ignore: [
+        '**/__tests__'
+    ]
+}
+
+const testConfig = {
     presets: [
         [
             '@babel/preset-env',
@@ -15,6 +34,13 @@ module.exports = {
                 }
             }
         ],
+        '@babel/preset-react',
         '@babel/preset-typescript'
-    ],
+    ]
 }
+
+const config = process.env.NODE_ENV === 'test'
+    ? testConfig
+    : buildConfig
+
+module.exports = config
