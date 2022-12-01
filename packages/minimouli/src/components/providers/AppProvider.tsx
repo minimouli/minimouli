45a1b2977d3e9828bb42c90f0cd442b8ea40b6c1
Adrien Belminksy <belminksy@gmail.com>
2022-12-01 18:15:20 +0100

    feat(minimouli): add custom auth provider

--- packages/minimouli/src/components/providers/AppProvider.tsx
+++ packages/minimouli/src/components/providers/AppProvider.tsx
@@ -7,6 +7,7 @@
 
 import { Box } from 'ink'
 import React from 'react'
+import { CustomAuthProvider } from './CustomAuthProvider.js'
 import { ErrorBoundary } from '../ErrorBoundary.js'
 import { Header } from '../Header.js'
 import { Upgradable } from '../Upgradable.js'
@@ -22,7 +23,9 @@ const AppProvider = ({ command, children }: AppProviderProps) => (
         <Header command={command} />
         <Upgradable />
         <ErrorBoundary>
-            {children}
+            <CustomAuthProvider>
+                {children}
+            </CustomAuthProvider>
         </ErrorBoundary>
     </Box>
 )
