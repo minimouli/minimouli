86649ac1b39374173e552d7ca998d42a84b42b66
Adrien Belminksy <belminksy@gmail.com>
2022-12-01 18:29:40 +0100

    refactor(framework): export blocks instead of declaring them globally

--- packages/framework/src/index.ts
+++ packages/framework/src/index.ts
@@ -5,34 +5,11 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import { afterAll } from './blocks/after-all.block.js'
-import { afterEach } from './blocks/after-each.block.js'
-import { beforeAll } from './blocks/before-all.block.js'
-import { beforeEach } from './blocks/before-each.block.js'
-import { expect } from './blocks/expect.block.js'
-import { sleep } from './blocks/sleep.block.js'
-import { suite } from './blocks/suite.block.js'
-import { test } from './blocks/test.block.js'
-import type { Expect, Hook, Sleep, Suite, Test } from '@minimouli/types/blocks'
-
-/* eslint-disable no-var */
-declare global {
-    var afterAll: Hook
-    var afterEach: Hook
-    var beforeAll: Hook
-    var beforeEach: Hook
-    var expect: Expect
-    var sleep: Sleep
-    var suite: Suite
-    var test: Test
-}
-/* eslint-enable */
-
-global.afterAll = afterAll
-global.afterEach = afterEach
-global.beforeAll = beforeAll
-global.beforeEach = beforeEach
-global.expect = expect
-global.sleep = sleep
-global.suite = suite
-global.test = test
+export * from './blocks/after-all.block.js'
+export * from './blocks/after-each.block.js'
+export * from './blocks/before-all.block.js'
+export * from './blocks/before-each.block.js'
+export * from './blocks/expect.block.js'
+export * from './blocks/sleep.block.js'
+export * from './blocks/suite.block.js'
+export * from './blocks/test.block.js'
