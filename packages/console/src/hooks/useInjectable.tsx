4e018066953206df4acf83c4de8ad62454cd71f1
Adrien Belminksy <belminksy@gmail.com>
2022-12-06 15:14:00 +0100

    refactor(console): add option to return a new instance in useInjectable

--- packages/console/src/hooks/useInjectable.tsx
+++ packages/console/src/hooks/useInjectable.tsx
@@ -9,12 +9,21 @@ import { useContext } from 'react'
 import { InjectableContext } from '../contexts/injectable.context.js'
 import type { Class } from '../types/class.type.js'
 
-const useInjectable = <T extends object>(token: Class<T>): T => {
+interface UseInjectableOptions {
+    createNewInstance: boolean
+}
+
+const useInjectable = <T extends object>(token: Class<T>, {
+    createNewInstance = false
+}: Partial<UseInjectableOptions> = {}): T => {
 
     const manager = useContext(InjectableContext)
 
     if (manager === undefined)
         throw new Error('Cannot resolve dependency')
+
+    if (createNewInstance)
+        return manager.instantiate(token)
 
     return manager.get(token)
 }
